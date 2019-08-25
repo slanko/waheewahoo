c16412cfcbe5fbc38cd777941ccd94979a4a0e17
slinky <QMBFXSL@gmail.com>
2019-08-26 09:56:15 +1200

    cloud time
    not cloud like the final fantasy man but more large collections of water vapour in the sky

--- New Unity Project/Assets/Scripts/vehiclescript.cs
+++ New Unity Project/Assets/Scripts/vehiclescript.cs
@@ -25,7 +25,7 @@ public class vehiclescript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKey(mvForward))
         {
